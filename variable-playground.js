c2b75f92642da88c27efe8e6860555a054eb16f8
Minh-Anh Pham <minhp13@gmail.com>
2016-01-15 11:04:23 -0500

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+// 	name: 'Minh',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Minh',
+// 	// 	age: 29
+// 	// }
+// 	obj.age = 29;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+
+var grades = [15, 88];
+
+function addGrades (gradesArr) {
+	gradesArr.push(55);
+	debugger;
+
+	// grades = [12, 13, 99];
+}
+
+addGrades(grades);
+console.log(grades);
