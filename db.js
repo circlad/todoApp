463f3a843757d6ec4d3992a7b5184df2bfb41401
Minh-Anh Pham <minhp13@gmail.com>
2016-01-18 02:02:12 -0500

    reset

--- db.js
+++ db.js
@@ -3,10 +3,19 @@
 // Exports the db object with has the todo mode, the sequelize instance and the sequelize library
 
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if (env === 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	});
+} else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
+}
 
 var db = {};
 
--- server.js
+++ server.js
@@ -95,11 +95,13 @@ app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
 	db.todo.create(body).then(function (todo) {
-		res.json(todo);
+		res.json(todo.toJSON());
 	}, function (e) {
 		res.status(400).json(e)
 	})
-	
+
+
+
 
 	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 	// 	return res.status(400).send();
