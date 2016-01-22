35da639c64209e6715d6e988cb931841af22789b
William Walcker <william@circlad.com>
2016-01-22 12:38:16 -0500

    JS file added

---
+++ public/js/app.js
@@ -0,0 +1,48 @@
+$("#loginPage").css("min-height", $(window).height());
+
+$(document).ready(function() {
+
+	$("#loginBox").submit(function(event) {
+
+		event.preventDefault();
+
+		var $email = $("#loginBox").find('input[name=email]')
+
+		var $password = $("#loginBox").find('input[name=password]')
+
+		// Check Email Validity
+
+		var errorMessage = ""
+
+		function isValidEmailAddress(emailAddress) {
+			var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+			return pattern.test(emailAddress);
+		};
+
+		if (!isValidEmailAddress($("#email").val())) {
+
+			errorMessage = "Please enter a valid email address.";
+
+		}
+
+		if (errorMessage == "") {
+
+			alert("Congrats! You're now registered!")
+
+			$.get('/users', function() {
+				$("body")
+					.append("email: "+ $email.val())
+					.append("password: "+ $password.val());
+			}, "json");
+
+
+
+		} else {
+
+			$("#error").html(errorMessage);
+
+		}
+
+	});
+
+})
