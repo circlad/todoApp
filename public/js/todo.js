978863688109b7feb4d44cafeec6c68172dc8b9e
Minh-Anh Pham <minhp13@gmail.com>
2016-01-27 09:57:16 -0500

    Update checkbox added

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,7 +1,7 @@
 // Resize content to browser-window
 $("#todoPage").css("min-height", $(window).height());
 
-// Go back to index (delete the authentication token)
+// Logout - Go back to index and delete the authentication token
 $("#logout").click(function() {
 
   var deleterUserLogin = {
@@ -19,7 +19,7 @@ $("#logout").click(function() {
     })
 
   // inits localStorage
-  localStorage.Auth = "";
+  localStorage.Auth = '';
 
   document.location.href = "index.html"
 });
@@ -40,22 +40,27 @@ var taskList = [];
 $.ajax(getTodolist)
   .done(function(data, statusText, xhr) {
     taskList = JSON.parse(xhr.responseText);
-    // console.log(taskList);
+    console.log(taskList);
 
     for (var i = 0; i < taskList.length; i++) {
-      $("#todoList").append('<li><input type="checkbox"/>' + taskList[i].description + '<button id=' + taskList[i].id + ' class="deleteButton"><img src="images/delete.png"></button></li>');
+      if (taskList[i].completed === true) {
+        $("#todoList").append('<li><input type="checkbox" id=' + taskList[i].id + ' name="completed" checked/>' + taskList[i].description + '<button id=' + taskList[i].id + ' class="deleteButton"><img src="images/delete.png"></button></li>');
+      } else {
+        $("#todoList").append('<li><input type="checkbox" id=' + taskList[i].id + ' name="completed"/>' + taskList[i].description + '<button id=' + taskList[i].id + ' class="deleteButton"><img src="images/delete.png"></button></li>');
+      }
+      
     }
 
   })
 
 function addTask(newTask) {
   taskList.push(newTask);
-  $("#todoList").append('<li><input type="checkbox"/>' + newTask.description + '<button id=' + newTask.id + ' class="deleteButton"><img src="images/delete.png"></button></li>');
+  $("#todoList").append('<li><input type="checkbox" id=' + newTask.id + ' name="completed" checked=' + newTask.completed + ' />' +  newTask.description + '<button id=' + newTask.id + ' class="deleteButton"><img src="images/delete.png"></button></li>');
   $("#newTask").val(""); //  réinitialiser l'input
 }
 
 // when the user adds a task
-$(addButton).click(function() {
+$('#addButton').click(function() {
 
   event.preventDefault();
 
@@ -90,6 +95,46 @@ $(addButton).click(function() {
     });
 })
 
+// When the user marks a task as completed
+$(document).on('click', 'input[name=completed]', function() {
+
+  var idToUpdate = $(this).attr('id');
+
+  var data = {
+    completed: false
+  }
+
+  if ($(this).prop('checked')) {
+    console.log('checked')
+    data.completed = true;
+    taskList[idToUpdate].completed = true;
+  } else {
+    console.log('unchecked');
+  }
+
+
+
+  var updateToDo = {
+    type: "PUT",
+    url: '/todos/' + idToUpdate,
+    data: JSON.stringify(data),
+    contentType: 'application/json',
+    dataType: 'json',
+    headers: {
+      Auth: localStorage.Auth
+    }
+  }
+
+  $.ajax(updateToDo)
+    .done(function(data, statusText, xhr) {
+      console.log('The task number ' + idToUpdate + ' was updated!')
+      console.log(xhr.responseText);
+    })
+
+
+
+})
+
 // When the user deletes a task
 function deleteTask(idToDelete) {
 
@@ -108,6 +153,7 @@ $(document).on('click', '.deleteButton', function() {
   // var $this = $(this);
   var idToDelete = $(this).attr('id');
 
+  // Setup the request to delete the task
   var deleteToDo = {
     type: 'DELETE',
     url: '/todos/' + idToDelete,
@@ -118,8 +164,9 @@ $(document).on('click', '.deleteButton', function() {
     }
   }
 
+  // Send a request to the server to delete the task
   $.ajax(deleteToDo)
-    .done(function(data, statusText, xhr) {
+    .done(function() {
       console.log('The task number ' + idToDelete + ' was deleted!');
 
       deleteTask(idToDelete);
